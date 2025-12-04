96735eaced9ee11973a23db1e8b94434e97da936
Zious <102797133+z-3515@users.noreply.github.com>
2025-12-04 20:56:11 +0000

    test

--- dist/meta.js
+++ dist/meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Maxx Custom Script
 // @namespace    maxx
-// @version      1.2
+// @version      1.3
 // @description  Maxx Script
 // @author       Maxx
+// @run-at       document-end
 // @match        *://*/*
+// @noframes
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/z-3515/Custom-SOC-Tools/main/dist/maxx.user.js
 // @downloadURL  https://raw.githubusercontent.com/z-3515/Custom-SOC-Tools/main/dist/maxx.user.js
